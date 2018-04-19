b66da45ad9c0ecef039629ac3d04d94b66be5554
Guillaume HUSTA <guillaume.husta@gmail.com>
2018-04-19 17:04:33 +0200

    Applying PEP 8 rule

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import time
 import redis
 from flask import Flask
 
-
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
@@ -24,6 +23,7 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
